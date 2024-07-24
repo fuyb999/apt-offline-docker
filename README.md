649d43031c18189ff4677e9522744cd51a8ce8f2
fuyb <656452024@qq.com>
2024-07-24 11:16:43 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-## get apt-offline.zip 
+# Ubuntu apt offline
+
+## get apt-offline.zip
+1. set the UBUNTU_VERSION environment variable in the .env file  
+2. set the PKG_DOWNLOAD_LIST environment variable in the .env file  
+3. get apt-offline.zip  
 ```shell
 sudo docker-compose up
 ```
@@ -7,5 +12,5 @@ sudo docker-compose up
 ```shell
 tar -zxvf apt-offline-1.8.5.tar.gz
 cd apt-offline
-./apt-offline install ../apt-offline.zip
+./apt-offline install <path>/apt-offline.zip
 ```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,25 +5,37 @@ RED='\033[00;31m'
 
 SAVE_PATH=/tmp/debs
 sed -i -E "s/(archive|security).ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
-apt-get update && apt install -y curl wget gpg python2
 
-list=(${PKG_DOWNLOAD_LIST// / })
-for pkg in ${list[@]}
-do
-  printf "$RED install $pkg...\n$RESTORE"
-  if [ "$pkg" == "nvidia-container-toolkit" ]; then
-    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-    && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
-    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-    apt-get update
-  fi
-  apt-get install -y $pkg
-done
+REQUIRE_PKG="curl wget gpg lsb_release add-apt-repository"
+apt-get update && apt-get install -y curl wget gpg lsb-core software-properties-common python3
+
+if [ -n "$(echo $PKG_DOWNLOAD_LIST | grep 'nvidia-container-toolkit')" ]; then
+   curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+   sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+   tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+fi
+
+if [ -n "$(echo $PKG_DOWNLOAD_LIST | grep 'docker-ce')" ]; then
+  curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
+  add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+fi
+
+if [ -n "$(echo $PKG_DOWNLOAD_LIST | grep 'google-chrome-stable')" ]; then
+  curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | tee /usr/share/keyrings/google-chrome.gpg > /dev/null
+  echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | tee /etc/apt/sources.list.d/google-chrome.list
+fi
+
+apt-get update
 
 rm -rf $SAVE_PATH/{apt-offline.sig,apt-offline.zip}
-# apt-offline在python2上运行正常，不可通过apt install
+# apt-offline依赖python3（通过apt install 安装的apt-offline，运行时报错）
 wget https://github.com/rickysarraf/apt-offline/releases/download/v1.8.5/apt-offline-1.8.5.tar.gz -O - | tar -zx
+
+# 卸载掉否则无法打包的apt-offline.zip中
+apt-get purge -y curl wget gpg lsb-core software-properties-common python3
+apt-get install -y python3
+
 cd apt-offline
 ./apt-offline set $SAVE_PATH/apt-offline.sig --update --upgrade --install-packages $PKG_DOWNLOAD_LIST
 ./apt-offline get $SAVE_PATH/apt-offline.sig --bundle $SAVE_PATH/apt-offline.zip
