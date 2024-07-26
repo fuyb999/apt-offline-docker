0ebdc64cde67daf2d1128c35c72a49c92a559e55
fuyb <656452024@qq.com>
2024-07-26 11:57:42 +0000

    update docker-entrypoint.sh.
    Signed-off-by: fuyb <656452024@qq.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt-get update && apt-get upgrade && apt-get install -y $PKG_DOWNLOAD_LIST
+# apt-get update && apt-get upgrade && apt-get install -y $PKG_DOWNLOAD_LIST
 
 rm -rf $SAVE_PATH/{apt-offline.sig,apt-offline.zip}
 
 apt-offline set --update --upgrade $SAVE_PATH/apt-offline.sig
-apt-offline set $SAVE_PATH/apt-offline.sig --install-packages $PKG_DOWNLOAD_LIST
+apt-offline set $SAVE_PATH/apt-offline.sig --update --upgrade --install-packages $PKG_DOWNLOAD_LIST
 apt-offline get $SAVE_PATH/apt-offline.sig --bundle $SAVE_PATH/apt-offline.zip
