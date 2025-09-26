60795957ab982c90e30bf938a29449eb316c32d7
fuyb <656452024@qq.com>
2025-09-26 22:39:38 +0800

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 SAVE_PATH=/tmp
 rm -rf $SAVE_PATH/* /etc/apt/sources.list.d/archive_uri-https_mirrors_ustc_edu_cn_docker-ce_linux_ubuntu-jammy.list
 
@@ -9,11 +11,14 @@ apt-get -y upgrade && \
   apt-offline get $SAVE_PATH/apt-offline.sig --bundle $SAVE_PATH/apt-offline.zip
 
 apt-get install -y dpkg-dev unzip
+
 unzip $SAVE_PATH/apt-offline.zip -d /tmp/apt-offline
 cd /tmp/apt-offline/ && dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz && cd -
+
 tee /etc/apt/sources.list.d/local.list << EOF
 deb [trusted=yes] file:/tmp/apt-offline ./
 EOF
+
 tar -Jcvf /tmp/apt-offline-mirror.tar.xz /tmp/apt-offline/ /etc/apt/sources.list.d/local.list
 
 tail -f /dev/null
